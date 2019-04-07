ffa92a3379e8b6e38006c24b79c7a08a276bccaa
ltaylor510 <ltaylor.jazz@gmail.com>
2019-04-06 23:49:51 -0700

    Update site.pp
    changing the owner  now

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,6 @@ node default {
    file {'/root/README':
    ensure  => file,
    content => 'Well what do you know! - more stuff added',
-   owner   => 'vagrant',
+   owner   => 'vagrant:vagrant',
  }
 }
